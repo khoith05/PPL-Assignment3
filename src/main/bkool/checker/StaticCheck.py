e49a83251bce126900375b6da5b3863554b6d5a4
KHOI <t.hoangkhoi312@gmail.com>
2021-10-19 02:44:43 +0700

    complete Redeclared check and 5 testcase

--- src/main/bkool/checker/StaticCheck.py
+++ src/main/bkool/checker/StaticCheck.py
@@ -14,13 +14,255 @@ class Symbol:
         self.mtype = mtype
         self.value = value
 
-class RedeclareCheck(BaseVisitor):pass
+
+# class Arraytype:
+#     def __init__(self,btype,size=0):
+#         self.btype=btype
+#         self.size=size
+#     def isArray():
+#         return size!=0
+
+class Var:
+    def __init__(self,name,bktype,isStatic,isConstant):
+        self.name=name
+        self.bktype=bktype
+        self.isstatic=isStatic
+        self.isconstant=isConstant
+    def __str__(self):
+        return "{ varname: "+self.name + ",type: "+self.bktype+",Static: "+self.isStatic+",Constant: "+self.isconstant+"}"
+
+
+class Func:
+    def __init__(self,name,bktype,partype,isStatic):
+        self.name=name
+        self.bktype=bktype
+        self.partype=partype
+        self.isstatic=isStatic
+    def __str__(self):
+        return "{ funcname: "+self.name+",rettype: "+self.bktype+",para: "+self.partype+",Static: "+self.isstatic+"}"
+
+class BKClass:
+    def __init__(self,name,parent,varlst,funclst):
+        self.name=name
+        self.parent=parent
+        self.varlst=varlst
+        self.funclst=funclst
+    def __str__(self):
+        return "{ classname: "+self.name+",parent: "+self.parent+",\nvarlist: "+self.varlst+",\nfunclist: "+self.funclst+"}"
+'''
+var= {
+    "name":str,
+    "type":,
+    "size":0,
+    "kind":
+}
+func={
+    "name":,
+    "rettype":,
+    "retsize":0,
+    "partype":,
+    "kind":
+}
+class={
+    "name":,
+    "parent":,
+    "varlst":,
+    "funclst":
+}
+global=[classlst]
+'''
+
+class RedeclareCheck(BaseVisitor):
+    
+    def visitProgram(self, ast, c):
+        global_envi=[]
+        # def checkclassdecl(classdecl):
+        #     global_envi+=classdecl.accept(self,global_envi)
+        list(map(lambda classdecl:classdecl.accept(self,global_envi),ast.decl))
+        return global_envi
+    
+    def visitVarDecl(self, ast, c):
+        #get var name
+        varname=ast.variable.accept(self,c)
+        vartype=ast.varType.accept(self,c)
+        #check redeclared
+        if varname in [var[0] for var in c] :
+            return varname
+        #add var 
+        c+=[[varname,vartype]]
+        return None
+
+    
+    def visitConstDecl(self, ast, c):
+        #get const name
+        constname=ast.constant.accept(self,c)
+        consttype=ast.constType.accept(self,c)
+        #check redeclared
+        if constname in [var[0] for var in c]:
+            return varname
+        c+=[[constname,consttype]]
+        return None
+    
+    def visitClassDecl(self, ast, c):
+        #get class name
+        classname=ast.classname.accept(self,c)
+        parent=ast.parentname.accept(self.c) if ast.parentname else None
+        # check redeclared class
+        print([eclass.name for eclass in c])
+        if classname in [eclass.name for eclass in c]:
+            raise Redeclared(Class(),classname)
+        #visit class
+        class_envi=BKClass(classname,ast.parentname,[],[])
+        list(map(lambda mem: mem.accept(self,class_envi),ast.memlist))
+        c+=[class_envi]
+    
+    def visitStatic(self, ast, c):
+        return True
+    
+    def visitInstance(self, ast, c):
+        return False
+    
+    def visitMethodDecl(self, ast, c):
+        #get method info
+        methodname=ast.name.accept(self,c)
+        rettype=ast.returnType.accept(self,c) if ast.returnType else None
+        #check redeclared method
+        if methodname in [efunc.name for efunc in c.funclst]+[evar.name for evar in c.varlst]:
+            raise Redeclared(Method() if rettype else SpecialMethod(),methodname)
+        #add method into class_envi
+        method_envi=Func(methodname,ast.returnType.accept(self,c),[],ast.kind.accept(self,c))
+        paralist=[]
+        def checkParam(para):
+            rev=para.accept(self,paralist)
+            if rev!=None:
+                raise Redeclared(Parameter(),rev)
+        list(map(checkParam ,ast.param))
+        method_envi.partype+=[para[1] for para in paralist]
+        c.funclst+=[method_envi]
+        ast.body.accept(self,paralist)
+
+
+    
+    def visitAttributeDecl(self, ast, c):
+        #check redeclared
+        varlst=[[var.name,var.bktype] for var in c.varlst]
+        rev=ast.decl.accept(self,varlst)
+        if rev!=None:
+            raise Redeclared(Attribute(),rev)
+        attr=Var(varlst[len(varlst)-1][0],varlst[len(varlst)-1][1],ast.kind.accept(self,c),isinstance(ast.decl,ConstDecl))
+        c.varlst+=[attr]
+
+    
+    def visitIntType(self, ast, c):
+        return IntType()
+    
+    def visitFloatType(self, ast, c):
+        return FloatType()
+    
+    def visitBoolType(self, ast, c):
+        return BoolType()
+    
+    def visitStringType(self, ast, c):
+        return StringType()
+    
+    def visitVoidType(self, ast, c):
+        return VoidType()
+    
+    def visitArrayType(self, ast, c):
+        return ArrayType(ast.size,ast.eleType)
+    
+    def visitClassType(self, ast, c):
+        return ast.classname.accept(self,c)
+    
+    def visitBinaryOp(self, ast, c):
+        return None
+    
+    def visitUnaryOp(self, ast, c):
+        return None
+    
+    def visitCallExpr(self, ast, c):
+        return None
+    
+    def visitNewExpr(self, ast, c):
+        return None
+    
+    def visitId(self, ast, c):
+        return ast.name
+    
+    def visitArrayCell(self, ast, c):
+        return None
+    
+    def visitFieldAccess(self, ast, c):
+        return None
+    
+    def visitBlock(self, ast, c):
+        #check redeclared
+        def checkDecl(decl):
+            rev=decl.accept(self,c)
+            if rev!=None:
+                raise Redeclared(Variable() if isinstance(decl,VarDecl) else Constant(),rev)
+        list(map(checkDecl,ast.decl))
+
+
+    
+    def visitIf(self, ast, c):
+        return None
+    
+    def visitFor(self, ast, c):
+        return None
+    
+    def visitContinue(self, ast, c):
+        return None
+    
+    def visitBreak(self, ast, c):
+        return None
+    
+    def visitReturn(self, ast, c):
+        return None
+    
+    def visitAssign(self, ast, c):
+        return None
+    
+    def visitCallStmt(self, ast, c):
+        return None
+    
+    def visitIntLiteral(self, ast, c):
+        return None
+    
+    def visitFloatLiteral(self, ast, c):
+        return None
+    
+    def visitBooleanLiteral(self, ast, c):
+        return None
+    
+    def visitStringLiteral(self, ast, c):
+        return None
+    
+    def visitNullLiteral(self, ast, c):
+        return None
+    
+    def visitSelfLiteral(self, ast, c):
+        return None 
+
+    def visitArrayLiteral(self, ast, c):
+        return None 
 
 class StaticChecker(BaseVisitor,Utils):
 
     global_envi = [
-    Symbol("getInt",MType([],IntType())),
-    Symbol("putIntLn",MType([IntType()],VoidType()))
+    BKClass("io",None,[],[
+        Func("readInt",IntType(),[],True),
+        Func("writeInt",VoidType(),[IntType()],True),
+        Func("writeIntLn",VoidType(),[IntType()],True),
+        Func("readFloat",FloatType(),[],True),
+        Func("writeFloat",VoidType(),[FloatType()],True),
+        Func("writeFloatLn",VoidType(),[FloatType()],True),
+        Func("readBool",BoolType(),[],True),
+        Func("writeBool",VoidType(),[BoolType()],True),
+        Func("writeBoolLn",VoidType(),[BoolType()],True),
+        Func("readStr",StringType(),[],True),
+        Func("writeStr",VoidType(),[StringType()],True),
+        Func("writeStrln",VoidType(),[StringType()],True),])
     ]
             
     
@@ -28,12 +270,12 @@ class StaticChecker(BaseVisitor,Utils):
         self.ast = ast
 
     def check(self):
-        return self.visit(self.ast,StaticChecker.global_envi)
-
-    
+        return self.visitProgram(self.ast,StaticChecker.global_envi)
 
     def visitProgram(self,ast, c): 
-        return [self.visit(x,c) for x in ast.decl]
+        global_class=ast.accept(RedeclareCheck(),StaticChecker.global_envi)
+        StaticChecker.global_envi+=global_class
+
 
 
 
--- src/test/CheckerSuite.py
+++ src/test/CheckerSuite.py
@@ -3,50 +3,583 @@ from TestUtils import TestChecker
 from AST import *
 
 class CheckerSuite(unittest.TestCase):
-    def test_undeclared_function(self):
-        """Simple program: int main() {} """
-        input = """int main() {foo();}"""
-        expect = "Undeclared Function: foo"
-        self.assertTrue(TestChecker.test(input,expect,400))
+    # def test_(self):
+    #     input="""
+    #       """
+    #     expect=""
+    #     self.assertTrue(TestChecker.test(input,expect,0))
 
-    def test_diff_numofparam_stmt(self):
-        """More complex program"""
-        input = """int main () {
-            putIntLn();
-        }"""
-        expect = "Type Mismatch In Statement: CallExpr(Id(putIntLn),List())"
+    def test_1(self):
+        input="""
+            class Add{
+                int a;
+                int a;
+            }
+
+        """
+        expect="Redeclared Attribute: a"
         self.assertTrue(TestChecker.test(input,expect,401))
-    
-    def test_diff_numofparam_expr(self):
-        """More complex program"""
-        input = """int main () {
-            putIntLn(getInt(4));
-        }"""
-        expect = "Type Mismatch In Expression: CallExpr(Id(getInt),List(IntLiteral(4)))"
+
+    def test_2(self):
+        input="""
+            class Add{
+                int a;
+            }
+            class Add{
+                int b;
+            }
+
+        """
+        expect="Redeclared Class: Add"
         self.assertTrue(TestChecker.test(input,expect,402))
 
-    def test_undeclared_function_use_ast(self):
-        """Simple program: int main() {} """
-        input = Program([FuncDecl(Id("main"),[],IntType(),Block([],[
-            CallExpr(Id("foo"),[])]))])
-        expect = "Undeclared Function: foo"
+    def test_3(self):
+        input="""
+            class Add{
+                int foo(){}
+                float foo(){}
+            }
+        """
+        expect="Redeclared Method: foo"
         self.assertTrue(TestChecker.test(input,expect,403))
 
-    def test_diff_numofparam_expr_use_ast(self):
-        """More complex program"""
-        input = Program([
-                FuncDecl(Id("main"),[],IntType(),Block([],[
-                    CallExpr(Id("putIntLn"),[
-                        CallExpr(Id("getInt"),[IntLiteral(4)])
-                        ])]))])
-        expect = "Type Mismatch In Expression: CallExpr(Id(getInt),List(IntLiteral(4)))"
+    def test_4(self):
+        input="""
+            class Add{
+                int foo;
+                float foo(){}
+            }
+        """
+        expect="Redeclared Method: foo"
         self.assertTrue(TestChecker.test(input,expect,404))
 
-    def test_diff_numofparam_stmt_use_ast(self):
-        """More complex program"""
-        input = Program([
-                FuncDecl(Id("main"),[],IntType(),Block([],[
-                    CallExpr(Id("putIntLn"),[])]))])
-        expect = "Type Mismatch In Statement: CallExpr(Id(putIntLn),List())"
+    def test_5(self):
+        input="""
+            class Add{
+                int foo(){
+                    int a,a;
+                }
+            }
+        """
+        expect="Redeclared Variable: a"
         self.assertTrue(TestChecker.test(input,expect,405))
+
+    '''def test_6(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,406))
+
+    def test_7(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,407))
+
+    def test_8(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,408))
+
+    def test_9(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,409))
+
+    def test_10(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,410))
+
+    def test_11(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,411))
+
+    def test_12(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,412))
+
+    def test_13(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,413))
+
+    def test_14(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,414))
+
+    def test_15(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,415))
+
+    def test_16(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,416))
+
+    def test_17(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,417))
+
+    def test_18(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,418))
+
+    def test_19(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,419))
+
+    def test_20(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,420))
+
+    def test_21(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,421))
+
+    def test_22(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,422))
+
+    def test_23(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,423))
+
+    def test_24(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,424))
+
+    def test_25(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,425))
+
+    def test_26(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,426))
+
+    def test_27(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,427))
+
+    def test_28(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,428))
+
+    def test_29(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,429))
+
+    def test_30(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,430))
+
+    def test_31(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,431))
+
+    def test_32(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,432))
+
+    def test_33(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,433))
+
+    def test_34(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,434))
+
+    def test_35(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,435))
+
+    def test_36(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,436))
+
+    def test_37(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,437))
+
+    def test_38(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,438))
+
+    def test_39(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,439))
+
+    def test_40(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,440))
+
+    def test_41(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,441))
+
+    def test_42(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,442))
+
+    def test_43(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,443))
+
+    def test_44(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,444))
+
+    def test_45(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,445))
+
+    def test_46(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,446))
+
+    def test_47(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,447))
+
+    def test_48(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,448))
+
+    def test_49(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,449))
+
+    def test_50(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,450))
+
+    def test_51(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,451))
+
+    def test_52(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,452))
+
+    def test_53(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,453))
+
+    def test_54(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,454))
+
+    def test_55(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,455))
+
+    def test_56(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,456))
+
+    def test_57(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,457))
+
+    def test_58(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,458))
+
+    def test_59(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,459))
+
+    def test_60(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,460))
+
+    def test_61(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,461))
+
+    def test_62(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,462))
+
+    def test_63(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,463))
+
+    def test_64(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,464))
+
+    def test_65(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,465))
+
+    def test_66(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,466))
+
+    def test_67(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,467))
+
+    def test_68(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,468))
+
+    def test_69(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,469))
+
+    def test_70(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,470))
+
+    def test_71(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,471))
+
+    def test_72(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,472))
+
+    def test_73(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,473))
+
+    def test_74(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,474))
+
+    def test_75(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,475))
+
+    def test_76(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,476))
+
+    def test_77(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,477))
+
+    def test_78(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,478))
+
+    def test_79(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,479))
+
+    def test_80(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,480))
+
+    def test_81(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,481))
+
+    def test_82(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,482))
+
+    def test_83(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,483))
+
+    def test_84(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,484))
+
+    def test_85(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,485))
+
+    def test_86(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,486))
+
+    def test_87(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,487))
+
+    def test_88(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,488))
+
+    def test_89(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,489))
+
+    def test_90(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,490))
+
+    def test_91(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,491))
+
+    def test_92(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,492))
+
+    def test_93(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,493))
+
+    def test_94(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,494))
+
+    def test_95(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,495))
+
+    def test_96(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,496))
+
+    def test_97(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,497))
+
+    def test_98(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,498))
+
+    def test_99(self):
+        input=""""""
+        expect=""
+        self.assertTrue(TestChecker.test(input,expect,499))'''
+
+
+
+    # def test_undeclared_function(self):
+    #     """Simple program: int main() {} """
+    #     input = """int main() {foo();}"""
+    #     expect = "Undeclared Function: foo"
+    #     self.assertTrue(TestChecker.test(input,expect,400))
+
+    # def test_diff_numofparam_stmt(self):
+    #     """More complex program"""
+    #     input = """int main () {
+    #         putIntLn();
+    #     }"""
+    #     expect = "Type Mismatch In Statement: CallExpr(Id(putIntLn),List())"
+    #     self.assertTrue(TestChecker.test(input,expect,401))
+    
+    # def test_diff_numofparam_expr(self):
+    #     """More complex program"""
+    #     input = """int main () {
+    #         putIntLn(getInt(4));
+    #     }"""
+    #     expect = "Type Mismatch In Expression: CallExpr(Id(getInt),List(IntLiteral(4)))"
+    #     self.assertTrue(TestChecker.test(input,expect,402))
+
+    # def test_undeclared_function_use_ast(self):
+    #     """Simple program: int main() {} """
+    #     input = Program([FuncDecl(Id("main"),[],IntType(),Block([],[
+    #         CallExpr(Id("foo"),[])]))])
+    #     expect = "Undeclared Function: foo"
+    #     self.assertTrue(TestChecker.test(input,expect,403))
+
+    # def test_diff_numofparam_expr_use_ast(self):
+    #     """More complex program"""
+    #     input = Program([
+    #             FuncDecl(Id("main"),[],IntType(),Block([],[
+    #                 CallExpr(Id("putIntLn"),[
+    #                     CallExpr(Id("getInt"),[IntLiteral(4)])
+    #                     ])]))])
+    #     expect = "Type Mismatch In Expression: CallExpr(Id(getInt),List(IntLiteral(4)))"
+    #     self.assertTrue(TestChecker.test(input,expect,404))
+
+    # def test_diff_numofparam_stmt_use_ast(self):
+    #     """More complex program"""
+    #     input = Program([
+    #             FuncDecl(Id("main"),[],IntType(),Block([],[
+    #                 CallExpr(Id("putIntLn"),[])]))])
+    #     expect = "Type Mismatch In Statement: CallExpr(Id(putIntLn),List())"
+    #     self.assertTrue(TestChecker.test(input,expect,405))
     
